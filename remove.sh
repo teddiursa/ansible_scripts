a954cf0c6495302fb125b22106af6aad94676c3f
teddi code server <coderteddi@gmail.com>
2024-08-27 17:03:16 +0000

    Adding new bootstrap play, bash script for easier removing, and deleting redundant remove plays

---
+++ remove.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Usage:
+# remove.sh <vimid1> [vimid2 ...]
+# Check if at least one argument is provided
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <vimid1> [<vimid2> ...]"
+    exit 1
+fi
+
+# Join all arguments into a single comma-separated string
+vimids=$(IFS=,; echo "$*")
+
+# Run the ansible-playbook command with the vimids
+ansible-playbook remove.yml --extra-var "vimids=[$vimids]"
