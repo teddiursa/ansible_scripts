ce05531bd7871019e0f1c4571314b60107a74c7d
teddi code server <coderteddi@gmail.com>
2024-08-27 21:17:25 +0000

    Adding more comments and removing redundant code for bootstrap play

--- remove.sh
+++ remove.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# Removes Containers or Virtual Machines with their correspoding ID's
+# Runs remove.yml as Ansible Play
+
 # Usage:
 # remove.sh <vimid1> [vimid2 ...]
+
 # Check if at least one argument is provided
 if [ $# -eq 0 ]; then
     echo "Usage: $0 <vimid1> [<vimid2> ...]"
